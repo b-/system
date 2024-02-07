c0092647fadffbce55d9aaaf68dd1a1899c184ae
bri <bri@bri-macbook>
2024-02-06 21:25:34 -0500

    add nil

--- devenv.nix
+++ devenv.nix
@@ -2,11 +2,11 @@
   self,
   inputs,
   pkgs,
-  lib,
   ...
 }: {
   packages = [
     pkgs.rnix-lsp
+    pkgs.nil
     self.packages.${pkgs.system}.pyEnv
     (inputs.treefmt-nix.lib.mkWrapper pkgs (import ./treefmt.nix))
   ];
