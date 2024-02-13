10044b2ea7fae7fa35c9346a64117acacf58e5be
bri <bri@bri-macbook>
2024-02-13 12:34:15 -0500

    add tailscale to darwin

--- modules/darwin/apps.nix
+++ modules/darwin/apps.nix
@@ -29,6 +29,7 @@
       "Jump Desktop" = 524141863;
       "Prompt" = 1594420480;
       "Refined GitHub" = 1519867270;
+      "Tailscale" = 1475387142;
       "TestFlight" = 899247664;
       "Termius" = 1176074088;
       "Apple Configurator" = 1037126344;
