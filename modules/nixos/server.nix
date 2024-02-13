cce1d5409036066321ed5cd5af998c1a92a58f9e
bri <bri@bri-macbook>
2024-02-13 01:14:03 -0500

    set server console to serial, enable guest agent

--- modules/nixos/server.nix
+++ modules/nixos/server.nix
@@ -1,1 +1,8 @@
-{ security.sudo.wheelNeedsPassword = false; }
+{
+  security.sudo.wheelNeedsPassword = false;
+  boot = {
+    growPartition = true;
+    kernelParams = [ "console=ttyS0" ];
+  };
+  services.qemuGuest.enable = lib.mkDefault true;
+}
