39b087456d270d6481c64e62a0a36cad056bb924
bri <bri@bri-macbook>
2024-02-13 06:06:10 -0500

    add build.sh

--- build.sh
+++ build.sh
@@ -1,95 +1,76 @@
 #!/usr/bin/env bash
+
+# set cachix user
 CACHIX_USER="bri"
+
+# only x86_64 for now
 ARCH=x86_64
 
-ENUMERATE_TARGETS(){
-#build_image_task:
-  #matrix:
-    # - <<: *ARM_CONTAINER_TEMPLATE
-    #   env:
-    #     ARCH: arm64
-    #     USER: root
+# only linux for now
+OS=linux
 
-    # Proxmox server image
-    #- <<: *LINUX_CONTAINER_TEMPLATE
-    PROXMOX_TARGET(){
-        :
-    }
-      env:
-        FORMAT: proxmox
-        DESTDIR: dump
-        EXT: vma.zst
-        TARGET: server
-    # raw server image
-    - <<: *LINUX_CONTAINER_TEMPLATE
-      env:
-        ARCH: x86_64
-        FORMAT: raw-efi
-        DESTDIR: images
-        EXT: img
-        TARGET: server
-    # iso server image
-    - <<: *LINUX_CONTAINER_TEMPLATE
-      env:
-        ARCH: x86_64
-        FORMAT: iso
-        DESTDIR: template/iso
-        EXT: iso
-        TARGET: server
-    # bri proxmox image
-    - <<: *LINUX_CONTAINER_TEMPLATE
-      env:
-        ARCH: x86_64
-        FORMAT: proxmox
-        DESTDIR: dump
-        EXT: vma.zst
-        TARGET: bri
-    # server lxc image (broken)
-    # - <<: *LINUX_CONTAINER_TEMPLATE
-    #   env:
-    #     ARCH: x86_64
-    #     FORMAT: proxmox-lxc
-    #     DESTDIR: template/cache
-    #     EXT: tar.xz
-    #     TARGET: server
+# hashtable of destdirs
+declare -A DESTDIRS=(
+  [proxmox]="dump"
+  [raw-efi]="images"
+  [iso]="template/iso"
+)
 
-    # bri raw image
-    - <<: *LINUX_CONTAINER_TEMPLATE
-      env:
-        ARCH: x86_64
-        FORMAT: raw-efi
-        EXT: img
-        DESTDIR: images
-        TARGET: bri
-    # bri iso image
-    - <<: *LINUX_CONTAINER_TEMPLATE
-      env:
-        ARCH: x86_64
-        FORMAT: iso
-        EXT: iso
-        TARGET: bri
-        DESTDIR: template/iso
+TARGETS=(
+  "server"
+  "bri"
+)
+FORMATS=(
+  "proxmox"
+  "raw-efi"
+  "iso"
+)
+declare -A EXTENSIONS=(
+  [raw-efi]="img"
+  [proxmox]="vma.zst"
+  [iso]="iso"
+)
+
+# filename prefixes (only proxmox needs this)
+declare -A PREFIXES=(
+  [proxmox]=vzdump-qemu-
+)
+
+_println(){
+  printf "%s\n" "${*}"
+}
+
+die(){
+  exit_code=254
+  if [[ "${1}" =~ ^[0-9]+$ ]] ; then
+    exit_code="${1}"
+    shift
+  fi
+  _println "${*}"
+  exit "${exit_code}"
 }
 
 INSTALL_CACHIX(){
     nix profile install github:nixos/nixpkgs/nixpkgs-unstable#cachix --impure && cachix use $CACHIX_USER
 }
+
+WITH_CACHIX(){
+  cachix watch-exec "${CACHIX_USER}" -- "${@}"
+}
+
 BUILD_IMAGE(){
     mkdir -p build
-    cp -LR "$(nix build ".#nixosConfigurations.${TARGET}@${ARCH//arm/aarch}-${CIRRUS_OS}.config.formats.${FORMAT}" --print-out-paths --show-trace --accept-flake-config)" build/
+    BUILD_FILE="$(nix build ".#nixosConfigurations.${TARGET}@${ARCH//arm/aarch}-${OS}.config.formats.${FORMAT}" --print-out-paths --show-trace --accept-flake-config)"
+    cp "${BUILD_FILE}" "build/"
 }
+
 LIST_RENAME_BUILD_ARTIFACTS(){
     set -x
     find build
-    if [[ "${EXT}" == "vma.zst" ]] ; then
-        for i in build/*.${EXT} ; do
-            mv "$i" build/"vzdump-qemu-${TARGET}_$(basename ${i})"
-        done
-    else
-        for i in build/*.${EXT} ; do
-            mv "$i" build/"${TARGET}_$(basename ${i})"
-        done
-    fi
+    OUTFILE="${PREFIXES[FORMAT]}${NAME}.${EXTENSIONS[$FORMAT]}"
+    for i in build/*."${EXT}" ; do
+        mv "$i" "build/${OUTFILE}"
+    done
 }
 
 SAVE_SSH_KEY(){
@@ -100,16 +81,27 @@ UPLOAD_ARTIFACTS(){
     set -x
     chmod 600 /tmp/ci-upload.key
     chmod -R 755 build
-    scp -C -i /tmp/ci-upload.key  -oStrictHostKeyChecking=no -oport=222 -oidentitiesonly=true -oPasswordAuthentication=no build/* ci-upload@home.ibeep.com:${DESTDIR}
+    scp -C -i /tmp/ci-upload.key  -oStrictHostKeyChecking=no -oport=222 -oidentitiesonly=true -oPasswordAuthentication=no build/* ci-upload@home.ibeep.com:"${DESTDIRS[${FORMAT}]}"
 }
 
+BUILD_IMAGES(){
+for FORMAT in "${FORMATS[@]}"; do
+  for TARGET in "${TARGETS[@]}"; do
+    echo "TARGET: ${TARGET}"
+    echo "FORMAT: ${FORMAT}"
+    BUILD_IMAGE_TARGET
+  done
+done
+}
 
 BUILD_IMAGE_TARGET(){
-#build_image_template: &BUILD_IMAGE_TEMPLATE
     NAME="build_image_${TARGET}@${ARCH}-${OS}_${FORMAT}"
-    INSTALL_CACHIX
+    #INSTALL_CACHIX
+    #WITH_CACHIX BUILD_IMAGE
     BUILD_IMAGE
     LIST_RENAME_BUILD_ARTIFACTS
     SAVE_SSH_KEY
     UPLOAD_ARTIFACTS
 }
+
+"${@}"
