e9029b9f79651fa49026874304549071891b1c8f
bri <bri@bri-macbook>
2024-02-14 15:53:54 -0500

    fix ci-screen.sh

--- ci-screen.sh
+++ ci-screen.sh
@@ -4,5 +4,5 @@ TAIL=(
     "+F"
     )
 rm -f ci.log
-screen -d -m -S build -L -Logfile ci.log bash -x ./build.sh CI-BUILD
+screen -d -m -S build -L -Logfile ci.log bash -x ./build.sh CI_BUILD
 "${TAIL[@]}" ci.log
