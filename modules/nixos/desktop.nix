31a258e498efaacac1c85d536a9e681ae5f90818
bri <bri@bri-macbook>
2024-02-16 19:25:47 -0500

    add 1password

--- modules/nixos/desktop.nix
+++ modules/nixos/desktop.nix
@@ -1,4 +1,9 @@
-{ pkgs, config, ... }:
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
 {
   # commented out because we import it at a higher hierarchical level
   #imports = [ ./keybase.nix ];
@@ -46,4 +51,24 @@
 
   # Electron applications use Wayland
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
+
+  # Enable select unfree packages
+  nixpkgs.config.allowUnfreePredicate =
+    pkg:
+    builtins.elem (lib.getName pkg) [
+      "1password"
+      "1password-cli"
+      "discord"
+      "google-chrome"
+      "vscode"
+    ];
+
+  programs._1password.enable = true;
+  programs._1password-gui = {
+    enable = true;
+    package = pkgs._1password-gui-beta;
+    # Certain features, including CLI integration and system authentication support,
+    # require enabling PolKit integration on some desktop environments (e.g. Plasma).
+    polkitPolicyOwners = [ "${config.user.name}" ];
+  };
 }
