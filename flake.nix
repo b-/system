f28a3c558015ad7cce8e3a452eeb6f843008aa0b
bri <bri@bri-macbook>
2024-02-18 19:08:43 -0500

    uh

--- flake.nix
+++ flake.nix
@@ -9,24 +9,25 @@
       "https://cache.nixos.org"
     ];
     allowed-uris = [
-      # "github:hercules-ci/" # flake-parts
-      # "github:serokell/" # deploy-rs
-      # "github:cachix/" # cachix & devenv
-      # "github:nix-community/"
-      # "github:nixos/"
-      # "github:Mic92/" # nix-index-database
-      # "github:numtide/" # nixos-anywhere
-      # "github:lnl7/" # nix-darwin
-      # "github:zhaofengli/" # attic
+      "github:hercules-ci/" # flake-parts
+      "github:serokell/" # deploy-rs
+      "github:cachix/" # cachix & devenv
+      "github:nix-community/"
+      "github:nixos/"
+      "github:Mic92/" # nix-index-database
+      "github:numtide/" # nixos-anywhere
+      "github:lnl7/" # nix-darwin
+      "github:zhaofengli/" # attic
+      "github:ipetkov/crane/"
 
-      # # me
-      # "github:b-/"
-      # "github:briorg/"
-      # "github:perchnet/"
+      # me
+      "github:b-/"
+      "github:briorg/"
+      "github:perchnet/"
 
-      "github:"
-      "git+https://github.com/"
-      "git+ssh://github.com/"
+      #"github:"
+      #"git+https://github.com/"
+      #"git+ssh://github.com/"
     ];
 
     trusted-public-keys = [
