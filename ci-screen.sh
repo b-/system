45eda7b7dafb12e725690eecebe26a1c4360fb90
bri <bri@bri-macbook>
2024-02-24 03:39:18 -0500

    add darwin targets

--- ci-screen.sh
+++ ci-screen.sh
@@ -6,6 +6,7 @@ DATE_TIME="$(DATE-TIME)"
 LOGFILE="$(mktemp "--suffix=${DATE_TIME}.log" ci)"
 TAIL=("tail" "-f")
 rm -f "${LOGFILE}"
+git pull --rebase | tee -a "${LOGFILE}"
 screen -d -m -S build -L -Logfile "${LOGFILE}" bash ./build.sh CI_BUILD
 # screen returns before the logfile is actually created, so test for race condition
 until [[ -f ${LOGFILE} ]]; do
