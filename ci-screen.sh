296c24df37c0bb14f13d8a835e6589689373458d
bri <bri@bri-macbook>
2024-02-24 03:19:54 -0500

    set logfile name dynamically

--- ci-screen.sh
+++ ci-screen.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
+DATE-TIME() {
+	printf %s "${DATE_TIME-$(date -I)-$(date +"%H-%M")}"
+}
+DATE_TIME="$(DATE-TIME)"
+LOGFILE="$(mktemp "--suffix=${DATE_TIME}.log" ci)"
 TAIL=("tail" "-f")
-rm -f ci.log
-screen -d -m -S build -L -Logfile ci.log bash ./build.sh CI_BUILD
-"${TAIL[@]}" ci.log
+rm -f "${LOGFILE}"
+screen -d -m -S build -L -Logfile "${LOGFILE}" bash ./build.sh CI_BUILD
+# screen returns before the logfile is actually created, so test for race condition
+until [[ -f ${LOGFILE} ]]; do
+	sleep 0.1s
+done
+"${TAIL[@]}" "${LOGFILE}"
