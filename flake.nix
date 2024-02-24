2250b49e5a1107d36983e9c9112f84a303a024db
bri <bri@bri-macbook>
2024-02-24 05:35:26 -0500

    add nix-community.cachix.org to substituters, upload logs

--- flake.nix
+++ flake.nix
@@ -6,6 +6,7 @@
       "https://bri.cachix.org"
       "https://perchnet.cachix.org"
       "https://devenv.cachix.org"
+      "https://nix-community.cachix.org"
       "https://cache.nixos.org"
     ];
     allowed-uris = [
@@ -36,6 +37,7 @@
       "perchnet.cachix.org-1:0mwmOwJFqL+r4HKl68GZ90ATTFsi3/L4ejSUIWaYYmc="
       "bri.cachix.org-1:/dk2nWYOEZl/BnC8h5CTKgao5HeWjCIgY1Tuj29Bq4s="
       "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
 
