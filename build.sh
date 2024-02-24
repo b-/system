4d0c1d54be107e8713e6569ba9795bd451788751
bri <bri@bri-macbook>
2024-02-24 05:25:00 -0500

    base file -> hash

--- build.sh
+++ build.sh
@@ -181,11 +181,11 @@ BUILD_IMAGE() { # Build image $TARGET@$ARCH-linux.$FORMAT
 		BUILT_FILE="$(nix build --print-out-paths "${NIX_BUILD_TARGET}" "${IMAGE_BUILD_FLAGS[@]}")"
 	fi
 
+	# BASE_FILE basename of $BUILT_FILE
+	BASE_FILE="$(basename "${BUILT_FILE}")"
 	# HASH first five bytes of $BASE_FILE
 	# e.g., `cHaRs`
 	HASH=$(cut -b-5 <<<"${BASE_FILE}")
-	# BASE_FILE basename of $BUILT_FILE
-	BASE_FILE="$(basename "${BUILT_FILE}")"
 	# CUT_FILE the rest of $BASE_FILE after the full $HASH
 	CUT_FILE="$(cut -d- -f2- <<<"${BASE_FILE}")"
 	# OUTNAME the final filename of our linked build artifact
