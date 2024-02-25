ded23e4353c82dfae63675a91e69493953ee02db
bri <bri@bri-macbook>
2024-02-24 23:59:09 -0500

    add gc step in last ditch effort to not run out of space :(

--- modules/nixos/gocd-agent.nix
+++ modules/nixos/gocd-agent.nix
@@ -3,7 +3,7 @@ let
   brain = "http://192.168.30.40:8153/go";
 in
 {
-  users.extraUsers.gocd-agent.extraGroups = [ "wheel" ];
+  users.extraUsers.gocd-agent.extraGroups = [ "wheel" ]; # yes this is crazy, but sudo nixos-rebuild is the dumb way i'm testing/playing with it...
   services.gocd-agent.enable = true;
   services.gocd-agent.goServer = brain;
   imports = [
