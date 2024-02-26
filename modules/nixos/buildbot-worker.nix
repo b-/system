2c0734e4078cdaf2f338aa1d8ec54aa5a67ae14c
bri <bri@bri-macbook>
2024-02-25 21:23:11 -0500

    add buildbot

--- modules/nixos/buildbot-worker.nix
+++ modules/nixos/buildbot-worker.nix
@@ -3,7 +3,8 @@ let
   brain = "192.168.30.40:9989";
 in
 {
-  users.extraUsers.buildbot-worker.extraGroups = [ "wheel" ]; # yes this is crazy, but sudo nixos-rebuild is the dumb way i'm testing/playing with it...
+  #users.extraUsers.buildbot-worker.extraGroups = [ "wheel" ]; # yes this is crazy, but sudo nixos-rebuild is the dumb way i'm testing/playing with it...
+  #users.users.buildbot-worker.isSystemUser = true;
   services.buildbot-worker.enable = true;
   services.buildbot-worker.masterUrl = brain;
 }
