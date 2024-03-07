c8a62b97d8452cc04b3ee764735e954ffde920a9
bri <bri@bri-macbook>
2024-03-07 18:12:31 -0500

    add some ssh keys and settings

--- modules/home-manager/ssh.nix
+++ modules/home-manager/ssh.nix
@@ -1,4 +1,8 @@
 { ... }:
+let
+  githubSshKeyPath = ".ssh/github.pub";
+  pveMacProSshKeyPath = ".ssh/pve-macpro.pub";
+in
 {
   programs.ssh = {
     enable = true;
@@ -7,5 +11,20 @@
     extraConfig = ''
       IdentitiesOnly true
     '';
+    matchBlocks = {
+      "default" = {
+        host = "*";
+        identitiesOnly = true;
+        identityFile = [ "~/${pveMacProSshKeyPath}" ];
+      };
+      "github" = {
+        host = "gh: github: github.com";
+        hostname = "github.com";
+        user = "git";
+        identityFile = "~/${githubSshKeyPath}";
+      };
+    };
   };
+  home.file."${githubSshKeyPath}".text = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKeIqs+mKkXHZ5XUdb8SJ8U2eqjiQLojRFnMciwafE29";
+  home.file."${pveMacProSshKeyPath}".text = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPDx+KV/SW4RGIeKA2FHU9S7bZgnJMy77N6lBeo2n8sJ";
 }
